5aacd363c65574da231d62879dc2ceb191eb6f8c
AugustSummer <1434691914@qq.com>
2020-05-31 16:28:13 +0800

    Refactor home [age view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -10,16 +10,25 @@ from lists.views import home_page
         self.assertEqual(1+1,3) '''
  
 class HomePageTest(TestCase):
-
+    '''
     def test_root_url_resolves_to_home_page_view(self):
         found=resolve('/') #resolve 是 Django 内部使用的函数，用于解析 URL，并将其映射到相应的视图函数上。
         self.assertEqual(found.func,home_page) #检查解析网站根路径“/”时，是否能找到名为 home_page 的函数。
            
     def test_home_page_returns_correct_html(self):
-        request=HttpRequest()
-        response=home_page(request)
+        #request=HttpRequest()
+        #response=home_page(request)
+        response=self.client.get('/')
         html=response.content.decode('utf8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-do lists</title>',html)
         self.assertTrue(html.endswith('</html>'))
+        #excepted_html=render_to_string('home.html')
+        #self.assertEqual(html,excepted_html)
+        self.assertTemplateUsed(response,'home.html')'''
+    
+    # 重构后代码
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response,'home.html')
     
--- lists/views.py
+++ lists/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-    return HttpResponse('<html><title>To-do lists</title></html>')
-
+    #return HttpResponse('<html><title>To-do lists</title></html>')
+    return render(request,'home.html')
